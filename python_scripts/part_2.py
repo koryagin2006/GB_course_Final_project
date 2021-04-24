ba5251baa783fd37087cf3b4f242c22ae8d78e31
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2021-04-24 15:38:31 +0500

    Update part_2.py

--- python_scripts/part_2.py
+++ python_scripts/part_2.py
@@ -6,7 +6,7 @@
 
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import StructType, StringType, StructField, IntegerType, DoubleType, FloatType, DateType
+from pyspark.sql.types import StringType, IntegerType, DoubleType, FloatType, DateType
 
 spark = SparkSession.builder.appName("gogin_spark").getOrCreate()
 
@@ -14,6 +14,7 @@ spark = SparkSession.builder.appName("gogin_spark").getOrCreate()
 data = spark.read \
     .options(delimiter=',', inferschema=True, header=True) \
     .csv(path="input_csv_for_recommend_system/data.csv")
+
 
 data.count()  # 20'000'003
 data.show(n=3)
@@ -82,16 +83,30 @@ only showing top 5 rows
 """
 
 # Удалить последние 3 строки в DF
-data = data \
-    .where(F.col('sale_date_date') != '(затронуто стр') \
-    .sort(F.col("sale_date_date").desc())
+data = data.where(F.col('sale_date_date') != '(затронуто стр')
 data.count()  # 20'000'000
 
 # TODO: Исправить типы данных на числовые (даты) где необходимо
+
 # Переведем sale_date_date в формат DateType
+data = data.withColumn(colName="sale_date_date", col=data["sale_date_date"].cast(DateType()))
+
+# Переведем contact_id, shop_id, product_id, product_sub_category_id, product_category_id, brand_id в формат IntegerType
 data = data \
-    .withColumn(colName="sale_date_date", col=data["sale_date_date"].cast(DateType()))
-data.printSchema()
+    .withColumn(colName="contact_id", col=data["contact_id"].cast(IntegerType())) \
+    .withColumn(colName="shop_id", col=data['shop_id'].cast(IntegerType())) \
+    .withColumn(colName='product_id', col=data['product_id'].cast(IntegerType())) \
+    .withColumn(colName='product_sub_category_id', col=data['product_sub_category_id'].cast(IntegerType())) \
+    .withColumn(colName='product_category_id', col=data['product_category_id'].cast(IntegerType())) \
+    .withColumn(colName='brand_id', col=data['brand_id'].cast(IntegerType()))
+
+# Переведем quantity в формат FloatType
+data = data.withColumn(colName='quantity', col=F.regexp_replace(str='quantity', pattern=',', replacement='.'))
+data = data.withColumn(colName='quantity', col=data['quantity'].cast(FloatType()))
 
 # TODO: Решить, что делать со значениями '-1'
+
+
 # TODO: Пересохранение файла в формат .parquet
+# data.write.parquet(path="input_csv_for_recommend_system/data.parquet")
+data.write.json(path="input_csv_for_recommend_system/data.json")
