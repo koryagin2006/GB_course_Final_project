4b81f4accb1a8b4ade55b5bfeb731f0ba7792209
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2021-04-25 22:37:27 +0500

    Update part_3.py

--- python_scripts/part_3.py
+++ python_scripts/part_3.py
@@ -4,7 +4,7 @@
 
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import IntegerType, FloatType, DateType
+from pyspark.sql.types import IntegerType
 
 spark = SparkSession.builder.appName("gogin_spark").getOrCreate()
 
@@ -12,4 +12,54 @@ spark = SparkSession.builder.appName("gogin_spark").getOrCreate()
 data = spark.read.parquet("input_csv_for_recommend_system/data.parquet")
 data.show(n=5, truncate=True)
 
+# Посмотрим диапазон дат
+data.select(
+    F.min('sale_date_date').alias('first_date'),
+    F.max('sale_date_date').alias('last_date'),
+    F.datediff(F.max('sale_date_date'), F.min('sale_date_date')).alias('datediff')
+).show(truncate=False)
 
+# Разряженность матрицы = 0.0333%
+n_users = data.select(F.countDistinct(col='contact_id')).collect()[0][0]
+n_items = data.select(F.countDistinct(col='product_id')).collect()[0][0]
+n_interactions = data.count()
+print(n_users, n_items, n_interactions)
+
+# популярность - группируем товары по сумме продаж
+popularity_by_items = data \
+    .groupBy("product_id").sum("quantity") \
+    .withColumnRenamed(existing='sum(quantity)', new='total_quantity')
+
+popularity_by_items.select('total_quantity').describe().show(truncate=False)
+popularity_by_items \
+    .orderBy('total_quantity', ascending=False) \
+    .withColumn(colName="total_quantity", col=popularity_by_items["total_quantity"].cast(IntegerType(), )) \
+    .show(n=5)
+
+# популярность - группируем пользователей по сумме продаж
+popularity_by_users = data \
+    .groupBy("contact_id").sum("quantity") \
+    .withColumnRenamed(existing='sum(quantity)', new='total_quantity')
+
+popularity_by_users.select('total_quantity').describe().show(truncate=False)
+popularity_by_users.orderBy('total_quantity', ascending=False) \
+    .withColumn(colName="total_quantity", col=popularity_by_users["total_quantity"].cast(IntegerType(), )) \
+    .show(n=5)
+
+# Введем колонку с номером недели
+data = data.withColumn('week_of_year', F.weekofyear(F.col('sale_date_date'))).select('sale_date_date', 'week_of_year')
+
+# Посмотрим диапазон неделей
+data.select(F.min('week_of_year'), F.max('week_of_year'),
+            (F.max('week_of_year') - F.min('week_of_year')).alias('week_diff')).show(truncate=False)
+
+
+def train_test_split_by_week(df, week_col_name, test_size_weeks):
+    threshold_week = int(data.select(F.max(week_col_name)).collect()[0][0]) - test_size_weeks
+    train = df.filter(F.col(week_col_name) < threshold_week)
+    test = df.filter(F.col(week_col_name) >= threshold_week)
+    return train, test
+
+
+data_train, data_test = train_test_split_by_week(df=data, week_col_name='week_of_year', test_size_weeks=3)
+data_test.show()
