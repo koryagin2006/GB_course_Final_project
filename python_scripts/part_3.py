28b315c3bcf6b14ba75a49d9426390dbd08e7665
Andrey Koryagin <carnen@mail.ru>
2021-04-26 17:50:45 +0500

    Update part_3.py

--- python_scripts/part_3.py
+++ python_scripts/part_3.py
@@ -70,3 +70,135 @@ def train_test_split_by_week(df, week_col_name, test_size_weeks):
 
 data_train, data_test = train_test_split_by_week(df=data, week_col_name='week_of_year', test_size_weeks=3)
 data_test.show()
+
+
+
+def popularity_recommendation(data, k=5):
+    """Топ-k популярных товаров"""
+    popularity_by_items = data \
+        .groupBy("product_id").sum("quantity") \
+        .withColumnRenamed(existing='sum(quantity)', new='sum_quantity') \
+        .orderBy('sum_quantity', ascending=False) \
+        .select('product_id') \
+        .head(n=k)
+    return [int(row.product_id) for row in popularity_by_items]
+
+popular_recs = popularity_recommendation(data=data_train, k=5)
+popular_recs
+
+
+
+data_train \
+    .groupBy("contact_id").pivot("product_id").sum("quantity") \
+    .show(n=5)
+# pyspark.sql.utils.AnalysisException: u'The pivot column product_id has more than 10000 distinct values, this could indicate an error. If this was intended, set spark.sql.pivotMaxValues to at least the number of distinct values of the pivot column.;'
+
+data.select(F.countDistinct('product_id')).show()
++--------------------------+
+|count(DISTINCT product_id)|
++--------------------------+
+|                     36549|
++--------------------------+
+
+train = data_train \
+    .withColumnRenamed('contact_id', 'user') \
+    .withColumnRenamed('product_id', 'item') \
+    .select('user', 'item', 'quantity') \
+    .groupBy('user', 'item').sum('quantity')
+
+test = data_test \
+    .withColumnRenamed('contact_id', 'user') \
+    .withColumnRenamed('product_id', 'item') \
+    .select('user', 'item', 'quantity') \
+    .groupBy('user', 'item').sum('quantity')
+
+
+
+'---------------------------------------------------------------------------'
+
+from pyspark.ml.recommendation import ALS
+from pyspark.sql import Row
+
+spark = SparkSession.builder.appName("gogin_spark").getOrCreate()
+
+df = spark.createDataFrame(
+	[(0, 0, 4.0), (0, 1, 2.0), (1, 1, 3.0), (1, 2, 4.0), (2, 1, 1.0), (2, 2, 5.0)], 
+	["user", "item", "rating"])
+df.show()
+
+"""
++----+----+------+
+|user|item|rating|
++----+----+------+
+|   0|   0|   4.0|
+|   0|   1|   2.0|
+|   1|   1|   3.0|
+|   1|   2|   4.0|
+|   2|   1|   1.0|
+|   2|   2|   5.0|
++----+----+------+
+"""
+
+als = ALS(rank=10, maxIter=5, seed=0)
+model = als.fit(dataset=df)
+model.rank  # 10
+
+model \
+	.userFactors \
+	.orderBy("id") \
+	.collect()  # [Row(id=0, features=[...]), Row(id=1, ...), Row(id=2, ...)]
+
+test = spark.createDataFrame([(0, 2), (1, 0), (2, 0)], ["user", "item"])
+test.show()
+"""
++----+----+
+|user|item|
++----+----+
+|   0|   2|
+|   1|   0|
+|   2|   0|
++----+----+
+"""
+predictions = sorted(model.transform(test).collect(), key=lambda r: r[0])
+
+predictions[0]  # Row(user=0, item=2, prediction=-0.13807615637779236)
+predictions[1]  # Row(user=1, item=0, prediction=2.6258413791656494)
+predictions[2]  # Row(user=2, item=0, prediction=-1.5018409490585327)
+
+user_recs = model.recommendForAllUsers(3)
+
+user_recs \
+    .where(user_recs.user == 0) \
+    .select("recommendations.item", "recommendations.rating") \
+    .collect()  # [Row(item=[0, 1, 2], rating=[3.910..., 1.992..., -0.138...])]
+
+item_recs = model.recommendForAllItems(3)
+item_recs \
+    .where(item_recs.item == 2) \
+    .select("recommendations.user", "recommendations.rating") \
+    .collect()  # [Row(user=[2, 1, 0], rating=[4.901..., 3.981..., -0.138...])]
+
+user_subset = df.where(df.user == 2)
+user_subset_recs = model.recommendForUserSubset(user_subset, 3)
+user_subset_recs \
+    .select("recommendations.item", "recommendations.rating") \
+    .first()  # Row(item=[2, 1, 0], rating=[4.901..., 1.056..., -1.501...])
+    
+item_subset = df.where(df.item == 0)
+item_subset_recs = model.recommendForItemSubset(item_subset, 3)
+item_subset_recs	\
+	.select("recommendations.user", "recommendations.rating") \
+	.first()  # Row(user=[0, 1, 2], rating=[3.910..., 2.625..., -1.501...])
+
+als_path = temp_path + "/als"
+als.save(als_path)
+als2 = ALS.load(als_path)
+als.getMaxIter()  # 5
+
+model_path = temp_path + "/als_model"
+model.save(model_path)
+model2 = ALSModel.load(model_path)
+model.rank == model2.rank  # True
+
+sorted(model.userFactors.collect()) == sorted(model2.userFactors.collect())  # True
+sorted(model.itemFactors.collect()) == sorted(model2.itemFactors.collect())  # True
