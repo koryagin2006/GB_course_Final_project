45cb898b5a9486e319d09176e6467f5867489b85
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2021-04-26 23:37:23 +0500

    Update part_5_predict.py

--- python_scripts/part_5_predict.py
+++ python_scripts/part_5_predict.py
@@ -12,8 +12,24 @@ model = MatrixFactorizationModel.load(sc, "ml_models/myCollaborativeFilter")
 
 
 # Предсказание
-def custom_predictions(user_id):
-    return [i.product for i in model.recommendProducts(user=user_id, num=5)]
+def custom_predictions(user_id, num_preds=5):
+    """
+    Предсказание
+    :param user_id: id покупателя
+    :param num_preds: количество рекомендуемых товаров
+    :return: отсортированный список из рекомендуемых товаров
+    """
+    return [i.product for i in model.recommendProducts(user=user_id, num=num_preds)]
 
 
-custom_predictions(user_id=886991)
+user_list = [903455, 983545, 1962163, 795400, 850395]
+
+for user in user_list:
+    print(custom_predictions(user_id=user, num_preds=5))
+"""
+[137539, 110629, 41096, 135427, 31212]
+[110629, 41096, 135427, 137539, 46797]
+[63439, 168308, 130230, 149540, 124837]
+[110629, 41096, 135427, 137539, 46797]
+[110629, 137539, 41096, 135427, 46797]
+"""
