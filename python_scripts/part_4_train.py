eff8faefbdaa301e828edc532db4f9dc9ec66278
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2021-05-02 21:45:39 +0500

    Update part_4_train.py

--- python_scripts/part_4_train.py
+++ python_scripts/part_4_train.py
@@ -1,19 +1,12 @@
 """
 /spark2.4/bin/pyspark
 """
-from pyspark.shell import sc
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel
-
-# from pyspark.ml.recommendation import ALS
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS
 
 spark = SparkSession.builder.appName("gogin_spark").getOrCreate()
-
-data = spark.read.parquet("input_csv_for_recommend_system/data.parquet")  # для начала готовим DataFrame
-
-# Введем колонку с номером недели
-data = data.withColumn(colName='week_of_year', col=F.weekofyear(F.col('sale_date_date')))
 
 
 def train_test_split_by_week(df, week_col_name, test_size_weeks):
@@ -22,9 +15,6 @@ def train_test_split_by_week(df, week_col_name, test_size_weeks):
     df_train = df.filter(F.col(week_col_name) < threshold_week)
     df_test = df.filter(F.col(week_col_name) >= threshold_week)
     return df_train, df_test
-
-
-data_train, data_test = train_test_split_by_week(df=data, week_col_name='week_of_year', test_size_weeks=3)
 
 
 def transform_for_als(df, user_col_name, item_col_name, rating_col_name):
@@ -42,12 +32,16 @@ def transform_for_als(df, user_col_name, item_col_name, rating_col_name):
         .withColumnRenamed(existing='sum(quantity)', new='quantity')
 
 
+data = spark.read.parquet("input_csv_for_recommend_system/data.parquet")  # для начала готовим DataFrame
+# Введем колонку с номером недели
+data = data.withColumn(colName='week_of_year', col=F.weekofyear(F.col('sale_date_date')))
+
+data_train, data_test = train_test_split_by_week(df=data, week_col_name='week_of_year', test_size_weeks=3)
 train = transform_for_als(data_train, 'contact_id', 'product_id', 'quantity')
 test = transform_for_als(data_test, 'contact_id', 'product_id', 'quantity')
 
 # Обучение
-model = ALS.trainImplicit(ratings=train, rank=10, alpha=0.01, nonnegative=True, seed=42)
-
+als = ALS(maxIter=5, regParam=0.1, userCol="contact_id", itemCol="product_id", ratingCol="quantity", implicitPrefs=True)
+model = als.fit(dataset=train)
 # Save model
-model.save(sc, "ml_models/myCollaborativeFilter")
-# sameModel = MatrixFactorizationModel.load(sc, "ml_models/myCollaborativeFilter")
+model.save(path="ml_models/my_als_2021-04-28")
