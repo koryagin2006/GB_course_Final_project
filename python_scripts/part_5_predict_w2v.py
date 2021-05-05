b81cccb32426aeb73bec05a1519c2e17dde83b3c
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2021-05-06 00:43:01 +0500

    Update part_5_predict_w2v.py

--- python_scripts/part_5_predict_w2v.py
+++ python_scripts/part_5_predict_w2v.py
@@ -3,26 +3,31 @@
 """
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import Word2Vec, Word2VecModel
+from pyspark.sql import functions as F
 
 spark = SparkSession.builder.appName("gogin_spark").getOrCreate()
+user_path = "hdfs://bigdataanalytics2-head-shdpt-v31-1-0.novalocal:8020/user/305_koryagin/"
 
 # Загрузка модели
-loadedWord2Vec = Word2Vec.load(path='ml_models/word2vec_2021_05_04')
-loadedModel = Word2VecModel.load(path='ml_models/word2vec-model_2021_05_04')
+loadedWord2Vec = Word2Vec.load(path='ml_models/word2vec_2021_05_05_by_prod_id')
+loadedModel = Word2VecModel.load(path='ml_models/word2vec-model_2021_05_05_by_prod_id')
+
+# Create DF
+products = spark \
+    .read.parquet(user_path + "input_csv_for_recommend_system/Product_dict.parquet") \
+    .withColumnRenamed(existing='__index_level_0__', new='product_id')
 
 
-def my_synonyms(model, word, num_synonyms=5):
+def my_synonyms(model, word, num_synonyms=5, ):
+    name = products.where(condition=F.col('product_id') == required_product_id).select('name').collect()[0]['name']
+    print('\n similar for %s' % name)
     return model \
-        .findSynonyms(word=word, num=num_synonyms) \
-        .select('word').withColumnRenamed(existing='word', new='similar for {}'.format(word))
+        .findSynonyms(word=str(word), num=num_synonyms) \
+        .select('word').withColumnRenamed(existing='word', new='product_id') \
+        .join(other=products, on='product_id', how='inner')
 
 
-loadedModel.getVectors().orderBy('word').show(n=100, truncate=False)
+# model.getVectors().show(n=30, truncate=False)
 
-
-required_products = ['(15835) Бинт марл мед стер 5м х 10см уп N1 671',
-					 '(179313) Жасмин масло эфирное 10мл 410',
-					 '(100178) Компливит Хондро тб №30 644']
-
-for p in required_products:
-	my_synonyms(model=loadedModel, word=p, num_synonyms=3).show(truncate=False)
+required_product_id = 44530
+my_synonyms(model=loadedModel, word=required_product_id, num_synonyms=3).show(truncate=False)
