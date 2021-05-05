b703647969aef3fa1d895c96a76d179a56713405
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2021-05-06 00:42:33 +0500

    Update part_1.sh

--- python_scripts/part_1.sh
+++ python_scripts/part_1.sh
@@ -4,4 +4,6 @@ hdfs dfs -rm -r input_csv_for_recommend_system/data.parquet
 
 hdfs dfs -put for_recomend_system/data.parquet input_csv_for_recommend_system
 
-hdfs dfs -ls ml_models
+hdfs dfs -rm -r ml_models/my_als_2021-04-28
+hdfs dfs -rm -r ml_models/my_als_2021-05-05_samlpe_20_percents
+hdfs dfs -rm -r ml_models/my_als_model_2021-05-05_samlpe_20_percents
