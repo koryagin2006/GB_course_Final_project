5c678ab0e7fccadbc10279af282d818ba74eb379
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2021-05-10 23:56:28 +0500

    Update part_5_predict_w2v.py

--- python_scripts/part_5_predict_w2v.py
+++ python_scripts/part_5_predict_w2v.py
@@ -2,14 +2,13 @@
 /spark2.4/bin/pyspark
 """
 from pyspark.sql import SparkSession
-from pyspark.ml.feature import Word2Vec, Word2VecModel
 from pyspark.sql import functions as F
+from pyspark.ml.feature import Word2Vec, Word2VecModel
 
 spark = SparkSession.builder.appName("gogin_spark").getOrCreate()
 user_path = "hdfs://bigdataanalytics2-head-shdpt-v31-1-0.novalocal:8020/user/305_koryagin/"
 
 # Загрузка модели
-loadedWord2Vec = Word2Vec.load(path='ml_models/word2vec_2021_05_05_by_prod_id')
 loadedModel = Word2VecModel.load(path='ml_models/word2vec-model_2021_05_05_by_prod_id')
 
 # Create DF
@@ -31,3 +30,13 @@ def my_synonyms(model, word, num_synonyms=5, ):
 
 required_product_id = 44530
 my_synonyms(model=loadedModel, word=required_product_id, num_synonyms=3).show(truncate=False)
+"""
+ similar for (3252) КЛОРАН Бэби вода очищающая мицеллярная 500мл 496
++----------+----------------------------------------------------------------------------------------------+
+|product_id|name                                                                                          |
++----------+----------------------------------------------------------------------------------------------+
+|133415    |(31478) Урьяж Термальная вода спрей 150мл №1 500                                              |
+|43981     |(120348) АВЕН ТРИКЗЕРА Бальзам для тела легкий питательный 200мл туба 547                     |
+|66238     |(118919) Фитосольба Фитологист 15 Средство комплексное против выпадения волос фл.3,5мл №12 478|
++----------+----------------------------------------------------------------------------------------------+
+"""
