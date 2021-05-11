ab0ddc53f61b64db209de46caa34fa5a785e31ae
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2021-05-11 22:59:37 +0500

    Update part_1.sh

--- python_scripts/part_1.sh
+++ python_scripts/part_1.sh
@@ -1,9 +1,17 @@
 hdfs dfs -du -h ml_models
-
+hdfs dfs -ls ml_models
 hdfs dfs -rm -r input_csv_for_recommend_system/data.parquet
 
 hdfs dfs -put for_recomend_system/data.parquet input_csv_for_recommend_system
 
-hdfs dfs -rm -r ml_models/my_als_2021-04-28
-hdfs dfs -rm -r ml_models/my_als_2021-05-05_samlpe_20_percents
-hdfs dfs -rm -r ml_models/my_als_model_2021-05-05_samlpe_20_percents
+
+hdfs dfs -rm -r ml_models/CV.model
+hdfs dfs -rm -r ml_models/IDF.model
+hdfs dfs -rm -r ml_models/myCollaborativeFilter
+hdfs dfs -rm -r ml_models/my_LR_model_8
+hdfs dfs -rm -r ml_models/my_LR_model_enrollees
+hdfs dfs -rm -r ml_models/word2vec_2021_05_05_by_prod_id
+hdfs dfs -rm -r ml_models/word2vec_2021_05_04
+hdfs dfs -rm -r ml_models/word2vec-model_2021_05_04
+hdfs dfs -rm -r ml_models/word2vec-model_2021_05_05_by_prod_id
+
