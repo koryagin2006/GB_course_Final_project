5eab64672b37855041533c134a4b64e510f610ad
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2021-05-11 22:59:57 +0500

    Update part_4_train_word2vec.py

--- python_scripts/model_Word2Vec/part_4_train_word2vec.py
+++ python_scripts/model_Word2Vec/part_4_train_word2vec.py
@@ -13,7 +13,6 @@ user_path = "hdfs://bigdataanalytics2-head-shdpt-v31-1-0.novalocal:8020/user/305
 data = spark.read.parquet(user_path + "input_csv_for_recommend_system/data.parquet")
 products = spark.read.parquet(user_path + "input_csv_for_recommend_system/Product_dict.parquet")
 
-
 # Data Preparation
 # convert the contact_id to StringType, conert the sale_date_date to DateType
 data = data \
@@ -28,7 +27,7 @@ print('Number of unique users = ' + str('{0:,}'.format(users.count()).replace(',
 
 # extract 90% of customer ID's
 (users_train, users_valid) = users.randomSplit(weights=[0.9, 0.1], seed=5)
-print(users_train.count(), users_valid.count())  # (1'473'217, 163'614)
+print(users_train.count(), users_valid.count())  # (1473'217, 163'614)
 
 # split data into train and validation set
 train_df = data.join(other=users_train, on='contact_id', how='inner')
@@ -39,26 +38,22 @@ print(train_df.count(), validation_df.count())  #
 # Введем колонку, определяющую номер чека и уберем лишние колонки
 def create_col_orders(df):
     return df \
-    .select(F.concat_ws('_', data.sale_date_date, data.shop_id, data.contact_id).alias('order_id'),
-            'product_id', 'quantity') \
-    .groupBy('order_id') \
-    .agg(F.collect_list(col='product_id')) \
-    .withColumnRenamed(existing='collect_list(product_id)', new='actual_products')
+        .select(F.concat_ws('_', data.sale_date_date, data.shop_id, data.contact_id).alias('order_id'),
+                'product_id', 'quantity') \
+        .groupBy('order_id') \
+        .agg(F.collect_list(col='product_id')) \
+        .withColumnRenamed(existing='collect_list(product_id)', new='actual_products')
 
 
 train_orders = create_col_orders(df=train_df)
 validation_orders = create_col_orders(df=validation_df)
 
-
 # TODO: Проработать необходимость. Отберем чеки с КПЧ меньше 10
 # number_of_positions_in_a_check = 10
-# orders_less = orders \
-#     .groupBy('order_id').count() \
+# orders_less = orders.groupBy('order_id').count() \
 #     .where(condition=F.col('count') < number_of_positions_in_a_check) \
 #     .select('order_id')
-
-# orders_filtered = orders \
-#     .join(other=orders_less, on='order_id', how='inner') \
+# orders_filtered = orders.join(other=orders_less, on='order_id', how='inner') \
 #     .withColumn(colName="product_id", col=orders["product_id"].cast(StringType()))  # Word2Vec принимает только str
 
 
