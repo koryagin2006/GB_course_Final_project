9f4b4dddecb7ec9fe1a4c33621f987d22c8cb867
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2021-05-13 00:11:44 +0500

    Update part_5_predict_ALS.py

--- python_scripts/model_ALS/part_5_predict_ALS.py
+++ python_scripts/model_ALS/part_5_predict_ALS.py
@@ -5,23 +5,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS, ALSModel
-from pyspark.sql.functions import col, explode
 from pyspark import SparkContext
 from pyspark.sql.types import IntegerType, FloatType, DateType, StructType, StructField, StringType
-
-from pyspark.sql import SparkSession
-
-# Import the required functions
-from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml.recommendation import ALS
 from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
-
-from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
-from pyspark.sql import functions as F
-
 import time
-
 
 spark = SparkSession.builder.appName("gogin_spark").getOrCreate()
 
