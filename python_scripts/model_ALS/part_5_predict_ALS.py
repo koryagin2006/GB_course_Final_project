df4e42cb0aa67ae5be1861d14e0bd9a113cd8cd9
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2021-05-14 01:55:48 +0500

    Update part_5_predict_ALS.py

--- python_scripts/model_ALS/part_5_predict_ALS.py
+++ python_scripts/model_ALS/part_5_predict_ALS.py
@@ -3,17 +3,37 @@
 """
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml.recommendation import ALS, ALSModel
-from pyspark import SparkContext
-from pyspark.sql.types import IntegerType, FloatType, DateType, StructType, StructField, StringType
-from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+from pyspark.ml.recommendation import ALSModel
 import time
 
 spark = SparkSession.builder.appName("gogin_spark").getOrCreate()
 
-# Load model
-model = ALSModel.load(path="ml_models/my_als_model_2021-05-05_samlpe_20_percents")
+user_path = "hdfs://bigdataanalytics2-head-shdpt-v31-1-0.novalocal:8020/user/305_koryagin/"
+model_als_path = "ml_models/my_als_model_2021-05-11_last_15_weeks.model_als"
 
-n_recommendations = model.recommendForAllUsers(numItems=5)
-n_recommendations.show(n=10)
+
+def load_model(user_path, model_path):
+    return ALSModel.load(path=user_path + model_path)
+
+
+# Load model_als and data
+model_als = load_model(user_path=user_path, model_path=model_als_path)
+data = spark.read.parquet(user_path + "input_csv_for_recommend_system/data.parquet")
+
+
+def predict_als(user_id, n_recs=3, model=model_als):
+    start = time.time()
+    preds_dict = {}
+    recs_list = model \
+        .recommendForAllUsers(numItems=n_recs) \
+        .where(condition=F.col('user_id') == user_id) \
+        .withColumn(colName="rec_exp", col=F.explode("recommendations")) \
+        .select(F.col("rec_exp.item_id"))
+    #
+    preds_dict['user_id'] = user_id
+    preds_dict['recommendations'] = [int(row.item_id) for row in recs_list.collect()]
+    preds_dict['prediction time'] = round(number=time.time() - start, ndigits=3)
+    return preds_dict
+
+
+print(predict_als(user_id=471, n_recs=3))
